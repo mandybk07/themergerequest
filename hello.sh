805b5759e9f6a0e1c8deaf34abd7a25fc9825f0b
mandybk07 <45913350+mandybk07@users.noreply.github.com>
2020-07-12 21:12:33 +0530

    Update hello.sh
    Updated hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,3 @@
-Cur_date=`date +%m/%d/%Y-%HH:MM`
-#echo $Cur_date
 echo "Hello World"
-echo "This is a shell script excuted from GIT Repo"
+echo "This is an example for shell script executed from GIT Repo"
 echo "Thank You"
